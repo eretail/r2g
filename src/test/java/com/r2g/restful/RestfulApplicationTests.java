22b26fcab4166b54a930948c03e2b78d7bff48b0
yinghcun li <yinghcun.li@welltok.com>
2019-11-11 21:48:40 -0500

    clean up the testing

--- src/test/java/com/r2g/restful/RestfulApplicationTests.java
+++ src/test/java/com/r2g/restful/RestfulApplicationTests.java
@@ -3,7 +3,7 @@ package com.r2g.restful;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+//@SpringBootTest
 class RestfulApplicationTests {
 
 	@Test
